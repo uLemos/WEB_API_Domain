c3e16bc2fe3f4494039a5e9f738113e1326f946e
Fernando Lemos <fsnvag12@gmail.com>
2023-06-12 19:10:34 -0300

    Web API Domain

---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Dominio
+{
+    public class User : IdentityUser<int> //Ao herdar de Identity, é possível criar no banco todas as tabelas padrões existentes.
+    {
+        public string NomeCompleto { get; set; } // NomeCompleto é uma tabela que está sendo inserida de maneira manual
+        public string Member { get; set; } = "Member";
+        public string OrgId { get; set; }
+        public List<UserRole> UserRoles { get; set; } //Lista de UserRoles
+    }
+}
---
+++ UserRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAPI.Dominio
+{
+    public class UserRole : IdentityUserRole<int> //relacionamento entre user e role 
+    {// UserID e RoleID precisam de um tipo para realizar o relacionamento.
+        public User User { get; set; }
+        public Role Role { get; set; }
+    }
+}
